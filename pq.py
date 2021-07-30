a9397ddc1112d46c64193542a5199aa3091f736a
jgsheppa <james.sheppard@avisio.ai>
2021-07-31 00:17:48 +0200

    Add exception and improve console messages

--- pq.py
+++ pq.py
@@ -1,11 +1,32 @@
+import time
+
+
+class colors:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = "\033[33m"
+    LIGHTGREY = "\033[37m"
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
+
+class EmptyQueueException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(colors.RED + self.message + colors.ENDC)
+
+
 class Item:
     def __init__(self, val, priority):
         self.val = val
         self.priority = priority
 
     def __repr__(self):
-        return "Item(val: {}, pri: {})".format(self.val,
-                                               self.priority)
+        val = colors.LIGHTGREY + "  {}".format(self.val) + colors.ENDC
+        arrow = colors.YELLOW + " -----> " + colors.ENDC
+        priority = colors.LIGHTGREY + "{}".format(self.priority) + colors.ENDC
+        queue = val + arrow + priority
+        return queue
 
 
 class PriorityQueue:
@@ -14,7 +35,15 @@ class PriorityQueue:
         self.storage = []
 
     def __str__(self):
-        return "[" + ", ".join([str(item) for item in self.storage]) + "]"
+        arrow = colors.YELLOW + "<--------------> \n" + colors.ENDC
+        priority_results = colors.BOLD + \
+            "Find the priority of each value in descending order:\n" \
+            + colors.ENDC + \
+            arrow + \
+            colors.LIGHTGREY + "Value -> Priority\n" + colors.ENDC + \
+            arrow + \
+            "\n".join([str(item) for item in self.storage])
+        return priority_results
 
     def standard_priority_func(self, p1, p2):
         return p1 < p2
@@ -31,25 +60,50 @@ class PriorityQueue:
         self.storage.insert(insert_index, to_insert)
 
     def peek(self):
+        if len(self.storage) == 1:
+            raise EmptyQueueException(
+                "Priority queue only has one value. There is nothing to peek at!")
         return self.storage[-1].val
 
     def pop(self):
+        if len(self.storage) == 0:
+            raise EmptyQueueException(
+                "Priority queue is empty. There is nothing to pop!")
         return self.storage.pop().val
 
 
 def simple_test():
-    print("Running simple test...")
+    print(colors.YELLOW + "Running simple test..." + colors.ENDC)
+    time.sleep(1)
     pq = PriorityQueue()
 
     pq.insert("c", 3)
     pq.insert("a", 1)
     pq.insert("b", 2)
 
+    print(pq)
+
     assert(pq.pop() == "a")
     assert(pq.pop() == "b")
     assert(pq.pop() == "c")
-    print("Simple test passed!")
+
+    print(colors.GREEN + "Simple test passed!" + colors.ENDC)
+
+
+def error_test():
+    print(colors.YELLOW + "Running test for error handling..." + colors.ENDC)
+    time.sleep(1)
+
+    pq = PriorityQueue()
+
+    pq.insert("c", 3)
+    pq.peek()
+
+    print('hey')
 
 
 if __name__ == "__main__":
     simple_test()
+    print("Initializing error test...")
+    time.sleep(1)
+    error_test()
